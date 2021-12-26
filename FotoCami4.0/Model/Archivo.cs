46c227613a91ad74c85a82fae0ab048e5c0e1ced
Enrique Wiedenmann <enrique.wiedenmann@marsh.com>
2021-12-26 17:09:35 -0300

    FotoCami4.0 primer commit version 1

---
+++ FotoCami4.0/Model/Archivo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FotoCami4._0.Model
+{
+    class Archivo
+    {
+        public string Origen { get; set; }
+        public string Destino { get; set; }
+        public string Nombre { get; set; }
+        public int Orden { get; set; }
+
+        internal string MeCopio(bool snOrden)
+        {
+            try
+            {
+
+                string destino;
+                FileInfo fi = new FileInfo(Origen + "/" + Nombre);
+                if (snOrden)
+                {
+                    destino = Destino + "/" + Orden + "_" + fi.Name;
+                }
+                else
+                {
+                    destino = Destino + "/" + fi.Name;
+                }
+
+                fi.CopyTo(destino);
+
+                return Nombre + "; copiado";
+            }
+            catch (Exception e)
+            {
+                return Nombre + "; " + e.Message;
+            }
+
+        }
+    }
+}
